f390a6207f972a62316e2193c612f9cca0fb3920
asarmy <asarmy@users.noreply.github.com>
2026-03-26 22:25:06 -0700

    Update toml and scratch file

--- scratch.py
+++ scratch.py
@@ -1,21 +1,22 @@
 # Note: this is just a scratch file for testing things out. It should be deleted eventually.
 
-# TODO: can I have a git centric TODO list?
-
 # TODO: tests take a while to run
-# TODO:
+# TODO: FaultSubsectionRuptures needs a cumul_mfd property
 
-# TODO: faultubsection needs to add length, width, area to the participating ruptures dataframe,
-# so that we can easily access those values for each rupture.
-# This will be useful for calculating the slip rate for each rupture,
-# which is needed for the slip rate model.  ha not really but close, good try ai
+#TODO: thinking ahead to ParentFault, it will need:
+# - coordinates (ordered so that dip is correct); how to deal with multilinestrings?
+# - some kind of list of the subsection indices and attributes (e.g., dip, width, etc. for each
+#   subsection within the parent fault)
+# - cumul mfds for all subsections (columns index, mag, cumul_rate)
+#
 
 
 from parserf.models import FaultModel, FaultModelDataset
+from parserf.subsection import FaultSubsection, FaultSubsectionData, FaultSubsectionRuptures
 
 dataset = FaultModelDataset(FaultModel.UCERF3_31)
 
-# sub = FaultSubsection(dataset, index=0)
+sub = FaultSubsection(dataset, index=0)
 # print(vars(sub.data))
 # print("sub.data.name:", sub.data.name)
 # print("sub.data.length_km:", sub.data.length_km)
@@ -24,7 +25,7 @@ dataset = FaultModelDataset(FaultModel.UCERF3_31)
 # print(vars(sub.ruptures))
 # print("sub.ruptures.participating_ruptures.head():", sub.ruptures.participating_ruptures.head())
 # print("sub.ruptures.participating_ruptures.tail():", sub.ruptures.participating_ruptures.tail())
-# print("sub.ruptures.participating_ruptures.columns:", sub.ruptures.participating_ruptures.columns)
+print("sub.ruptures.participating_ruptures.columns:", sub.ruptures.participating_ruptures.columns)
 # print("/n/n")
 
 # sub2=FaultSubsectionData(dataset, index=0)
